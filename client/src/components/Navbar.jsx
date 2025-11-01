462bc9ec35760628180dd793789ac7e94dc0371f
Arman Redhu <armanredhu55@gmail.com>
2025-11-01 16:10:16 +0530

    change in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   const { openSignIn } = useClerk()
 
   return (
-    <div className='fixed z-5 w-full background-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer'>
+    <div className='fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer'>
         <img src={assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer' onClick={() => navigate('/')} />
         {
           user ? <UserButton /> 
